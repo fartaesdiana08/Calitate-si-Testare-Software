0c21e69f25655643856581a1b72e965af499b670
fartaesdiana08 <diana.fartaes@gmail.com>
2021-03-04 09:22:10 +0200

    seminar 2 final

--- CTSseminar2/src/ro/ase/cts/seminar2/Account.java
+++ CTSseminar2/src/ro/ase/cts/seminar2/Account.java
@@ -1,11 +1,14 @@
 package ro.ase.cts.seminar2;
 
+import ro.ase.cts.seminar2.exceptii.IllegalTransferException;
+import ro.ase.cts.seminar2.exceptii.InsufficientFundsException;
+
 public abstract class Account {
 	//clasa nu va putea fi instantiata
 	//este folosita ca sablon
 	public abstract void deposit(double amount);
-	public abstract void withdraw(double amount);
-	public abstract void transfer(double amount, Account destination);
+	public abstract void withdraw(double amount) throws InsufficientFundsException;
+	public abstract void transfer(double amount, Account destination) throws IllegalTransferException, InsufficientFundsException;
 	public abstract double getBalance();
 
 }
--- CTSseminar2/src/ro/ase/cts/seminar2/Main.java
+++ CTSseminar2/src/ro/ase/cts/seminar2/Main.java
@@ -1,11 +1,28 @@
 package ro.ase.cts.seminar2;
 
+import ro.ase.cts.seminar2.exceptii.IllegalTransferException;
+import ro.ase.cts.seminar2.exceptii.InsufficientFundsException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//CurrentAccount c=new CurrentAccount(300, "IBAN1");
-		//System.out.println("suma disponibila este: "+c.getBalance());
+		CurrentAccount c=new CurrentAccount(300, "IBAN1");
+		CurrentAccount account2= new CurrentAccount(200, "IBAN2");
+		
+		System.out.println("suma disponibila este: "+c.getBalance());
 		System.out.println("Creditul maxim pentru cont curent este "+CurrentAccount.MAX_CREDIT);
+		c.deposit(200);
+		System.out.println("suma disponibila este: "+c.getBalance());
+		try{
+			c.withdraw(200);
+			c.transfer(100, c);
+		}catch(InsufficientFundsException e) {
+			System.err.println(e.getMessage());
+		} catch (IllegalTransferException e) {
+			e.printStackTrace();
+		}
+		System.out.println("suma disponibila este: "+c.getBalance());
+		System.out.println("suma in contul 2 este: "+account2.getBalance());
 	}
 
 }

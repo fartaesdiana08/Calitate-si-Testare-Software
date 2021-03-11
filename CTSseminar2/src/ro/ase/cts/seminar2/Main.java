e243205610403a3d9db41f094dc14555df1e80a1
fartaesdiana08 <diana.fartaes@gmail.com>
2021-03-11 08:20:10 +0200

    seminar 3 part 1

--- CTSseminar2/src/ro/ase/cts/seminar2/Main.java
+++ CTSseminar2/src/ro/ase/cts/seminar2/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		CurrentAccount c=new CurrentAccount(300, "IBAN1");
 		CurrentAccount account2= new CurrentAccount(200, "IBAN2");
 		
+		SavingsAccount account3=new SavingsAccount(300, "IBAN");
+		
 		System.out.println("suma disponibila este: "+c.getBalance());
 		System.out.println("Creditul maxim pentru cont curent este "+CurrentAccount.MAX_CREDIT);
 		c.deposit(200);
@@ -23,6 +25,12 @@ public class Main {
 		}
 		System.out.println("suma disponibila este: "+c.getBalance());
 		System.out.println("suma in contul 2 este: "+account2.getBalance());
+		System.out.println("suma in contul 3 este: "+account3.getBalance());
+		account3.addInterest(10);
+		System.out.println("suma in contul 3 este"+account3.getBalance());
+	
+		Bank banca=new Bank();
+		BankAccount account4=banca.openBankAccount(AccountType.CURRENT);
 	}
 
 }
